d9d01e2e34de412e2bfb6447f96d3ecbd58d18fe
Oliur Rahman <62242483+oliur-portfolio@users.noreply.github.com>
2024-10-01 05:19:48 +0600

    new page

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  function initScrollBlurEffect(section) {
+    const cardContainer = section.querySelector(".custom__scrollbar--slide");
+    const blurGradient = section.querySelector(".blur__gradient--slide");
 
+    if (!cardContainer || !blurGradient) return;
+
+    let lastScrollLeft = 0;
+
+    cardContainer.addEventListener("scroll", () => {
+      const scrollLeft = cardContainer.scrollLeft;
+
+      if (scrollLeft > lastScrollLeft) {
+        blurGradient.classList.add("hide");
+      } else {
+        blurGradient.classList.remove("hide");
+      }
+
+      lastScrollLeft = scrollLeft;
+    });
+  }
+
+  document.querySelectorAll(".slide__scroll-section").forEach((section) => {
+    initScrollBlurEffect(section);
+  });
+});
