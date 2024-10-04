d902ef920b3f0b4763418043552b8a50ef4bb6e1
Oliur Rahman <62242483+oliur-portfolio@users.noreply.github.com>
2024-10-04 10:17:18 +0600

    new pages

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Blur Scroll Card
   function initScrollBlurEffect(section) {
     const cardContainer = section.querySelector(".custom__scrollbar--slide");
     const blurGradient = section.querySelector(".blur__gradient--slide");
@@ -23,4 +24,25 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".slide__scroll-section").forEach((section) => {
     initScrollBlurEffect(section);
   });
+
+  // Submenu Show
+  const categoryLink = document.querySelector(".nav__link--category");
+  const submenuCategory = document.querySelector(".submenu__category");
+  const headerArea = document.querySelector(".header");
+
+  if (categoryLink) {
+    categoryLink.addEventListener("click", function (event) {
+      event.preventDefault();
+      submenuCategory.classList.toggle("submenu__category--show");
+    });
+
+    document.addEventListener("click", function (event) {
+      if (
+        !headerArea.contains(event.target) &&
+        submenuCategory.classList.contains("submenu__category--show")
+      ) {
+        submenuCategory.classList.remove("submenu__category--show");
+      }
+    });
+  }
 });
